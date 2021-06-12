d7632d2a92f5d0faf40b2e2bd3516c08df2ab177
kavinda-ravishan <kvnravishan@outlook.com>
2021-06-12 16:57:08 +0530

    part 1

---
+++ Web_Dev_Simplified/part_1.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE test; # Create a database with name test
+DROP DATABASE test; # delete the database
---
+++ Web_Dev_Simplified/part_2.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE record_company;
+USE record_company; # select a database for run feature queries
