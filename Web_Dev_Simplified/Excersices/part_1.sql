4bc55cfde58190c1e16e22796cd28bb899a6bb7e
kavinda-ravishan <kvnravishan@outlook.com>
2021-06-18 17:19:56 +0530

    sql ex final

--- Web_Dev_Simplified/Excersices/part_1.sql
+++ Web_Dev_Simplified/Excersices/part_1.sql
@@ -41,3 +41,31 @@ SELECT bands.name AS 'Band Name' FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
 HAVING COUNT(albums.id) = 0;
+
+SELECT albums.name as Name, 
+albums.release_year as 'Release Year', 
+SUM(songs.length) as 'Duration'
+FROM albums
+JOIN songs on albums.id = songs.album_id
+GROUP BY songs.album_id
+ORDER BY Duration DESC
+LIMIT 1;
+
+UPDATE albums
+SET release_year = 1986
+WHERE id = 4;
+
+SELECT AVG(length) as 'Average Song Duration' FROM songs;
+
+SELECT albums.name AS 'Album',
+albums.release_year AS 'Release Year',
+MAX(songs.length) AS 'Duration'
+FROM albums
+JOIN songs ON albums.id = songs.album_id
+GROUP BY songs.album_id;
+
+SELECT bands.name AS 'Band', COUNT(songs.id) AS 'Number of Songs'
+FROM bands
+JOIN albums ON bands.id = albums.band_id
+JOIN songs ON albums.id = songs.album_id
+GROUP BY albums.band_id;
